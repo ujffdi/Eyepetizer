1864fe3b5f04c286638bf7e620d07fac2308e42c
Tongsr <1009956987@qq.com>
2023-04-24 13:32:12 +0800

    引入multitype

---
+++ lib/common/src/main/java/com/tongsr/common/TestCommon.kt
@@ -0,0 +1,9 @@
+package com.tongsr.common
+
+/**
+ * @author Tongsr
+ * @date 2023/4/24
+ * @description
+ */
+class TestCommon {
+}
---
+++ lib/common/src/main/java/com/tongsr/common/multitype/ViewHolderInflater.kt
@@ -0,0 +1,8 @@
+package com.tongsr.common.multitype
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author Drakeet Xu
+ */
+abstract class ViewHolderInflater<T, VH : RecyclerView.ViewHolder> : ItemViewBinder<T, VH>()
